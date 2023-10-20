d7e38ec339642656e0d3118b5e4cb6c7d856855c
Juan Mañanes <121260905+mrs4ndman@users.noreply.github.com>
2023-10-20 15:46:34 +0200

    tweak: main page title + CNAME

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'mrs4ndman.dev';
-export const SITE_DESCRIPTION = 'A .dev site where I keep my portfolio and dev stuff';
+export const SITE_TITLE = 'blog.mrs4ndman.dev';
+export const SITE_DESCRIPTION = 'A blog site where I keep my portfolio and dev stuff';
